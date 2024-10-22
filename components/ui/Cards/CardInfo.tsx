623c5c870c24e3358569409e53cffd8aa7c02d73
Ezequiel Rey <ezequielrey@MacBook-Air-de-Ezequiel.local>
2024-10-22 15:48:33 -0300

    changes

--- components/ui/Cards/CardInfo.tsx
+++ components/ui/Cards/CardInfo.tsx
@@ -17,9 +17,25 @@ const CardInfo: React.FC<CardInfoProps> = ({
   buttonLink,
 }) => {
   return (
-    <div className="relative w-full max-w-xs min-h-[200px] h-auto overflow-hidden rounded-2xl border border-gray-600 p-6 flex flex-col">
-      <div className="flex h-full w-full items-start gap-2 text-left">
-        <div className="flex h-10 w-10 justify-center rounded-xl border border-gray-600 p-2 text-white">
+    <div className="relative w-full max-w-xs min-h-[200px] h-auto overflow-hidden rounded-2xl border border-gray-800 p-6 flex flex-col">
+      {/* Linea en pos absoluta situada en left-1/2 que es la mitad con un top en 0 */}
+      <div
+        aria-hidden="true"
+        className="left-1/2 top-0 w-[300px] center pointer-events-none absolute h-1 max-w-full -translate-x-1/2 -translate-y-1/2"
+        style={{
+          background: `linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(255, 255, 255, 0) 0%, #FBD38D 50%, rgba(0, 0, 0, 0) 100%)`,
+        }}
+      ></div>
+      <div
+        aria-hidden="true"
+        className="left-1/2 bottom-0 w-[300px] center pointer-events-none absolute h-px max-w-full -translate-x-1/2 -translate-y-1/2"
+        style={{
+          background: `linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(255, 255, 255, 0) 0%, #FBD38D 50%, rgba(0, 0, 0, 0) 100%)`,
+        }}
+      ></div>
+
+      <div className="flex h-full w-full items-start gap-4 text-left">
+        <div className="flex h-10 w-10 justify-center rounded-xl border border-gray-800 p-2 text-white">
           <Icon className="w-6 h-6" />
         </div>
         <div className="flex flex-col">
