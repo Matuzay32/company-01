570dd17fde977ad67f70f0f5326c5ce003fb586b
Ezequiel Rey <ezequielrey@Air-de-Ezequiel.fibertel.com.ar>
2024-10-23 16:34:47 -0300

    changes

--- components/3d/CubeCanvas.jsx
+++ components/3d/CubeCanvas.jsx
@@ -20,15 +20,25 @@ const Model = () => {
     }
   });
 
-  const materialProps = useControls({
-    scale: { value: 1, min: 0.1, max: 10, step: 0.05 },
-    thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
-    roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-    transmission: { value: 1, min: 0, max: 1, step: 0.1 },
-    ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
-    chromaticAberration: { value: 0.02, min: 0, max: 1 },
-    backside: { value: true },
-  });
+  // const materialProps = useControls({
+  //   scale: { value: 3, min: 0.1, max: 10, step: 0.05 },
+  //   thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
+  //   roughness: { value: 0, min: 0, max: 1, step: 0.1 },
+  //   transmission: { value: 1, min: 0, max: 1, step: 0.1 },
+  //   ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
+  //   chromaticAberration: { value: 0.02, min: 0, max: 1 },
+  //   backside: { value: true },
+  // });
+
+  const materialProps = {
+    scale: 3.9,
+    thickness: 0.2,
+    roughness: 0,
+    transmission: 1,
+    ior: 1.2,
+    chromaticAberration: 0.02,
+    backside: true,
+  };
 
   // Escalar el tamaño del texto según el viewport
   const textSize = Math.max(0.4, viewport.width / 1000); // Ajustar el divisor según sea necesario
@@ -60,6 +70,10 @@ const Model = () => {
       <mesh ref={torus} {...nodes.Torus002}>
         <MeshTransmissionMaterial {...materialProps} />
       </mesh>
+      {/* <mesh ref={cube}> */}
+      {/* <boxGeometry args={[1, 1, 1]} /> Crear un cubo */}
+      {/* <MeshTransmissionMaterial {...materialProps} /> */}
+      {/* </mesh> */}
     </group>
   );
 };
