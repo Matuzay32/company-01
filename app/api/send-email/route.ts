f9a489796d6eb5325a282f2e169142f62b04421e
Lucas <lucas.digenova@darwoft.com>
2025-03-01 21:40:16 -0300

    api sengrid

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,34 +1,50 @@
-import { NextRequest, NextResponse } from "next/server";
+import sgMail, { MailDataRequired } from "@sendgrid/mail";
 
-import sgMail from "@sendgrid/mail";
+import { NextResponse } from "next/server";
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
   try {
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
-      return NextResponse.json({ message: "Missing fields" }, { status: 400 });
+      return NextResponse.json(
+        { error: "All fields are required." },
+        { status: 400 }
+      );
     }
 
-    const msg = {
-      to: "destinatario@example.com", // Cambia a tu correo de destino
-      from: "tu_correo@example.com", // Cambia a tu correo verificado en SendGrid
-      subject: `Nuevo mensaje de contacto de ${name}`,
-      text: `Mensaje: ${message}\nDe: ${name} <${email}>`,
+    const senderEmail = process.env.SENDGRID_SENDER_EMAIL;
+    const toEmail = process.env.SENDGRID_TO_EMAIL;
+
+    if (!senderEmail || !toEmail) {
+      return NextResponse.json(
+        { error: "Missing email configuration." },
+        { status: 500 }
+      );
+    }
+
+    const msg: MailDataRequired = {
+      to: toEmail,
+      from: senderEmail,
+      subject: `New Contact Form Submission from ${name}`,
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+      html: `<p><strong>Name:</strong> ${name}</p>
+                   <p><strong>Email:</strong> ${email}</p>
+                   <p><strong>Message:</strong> ${message}</p>`,
     };
 
     await sgMail.send(msg);
 
     return NextResponse.json(
-      { message: "Email sent successfully" },
+      { message: "Email sent successfully!" },
       { status: 200 }
     );
-  } catch (error: any) {
-    console.error("Error sending email:", error.message);
+  } catch (error) {
+    console.error("Error sending email:", error);
     return NextResponse.json(
-      { message: "Error sending email", error: error.message },
+      { error: "Failed to send email" },
       { status: 500 }
     );
   }
