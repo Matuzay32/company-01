eac4dfecba2a92a7ac5086f3f806cdf17d35f9ff
Matuzay32 <66391796+Matuzay32@users.noreply.github.com>
2025-03-13 16:14:44 -0300

    Update next.config.js
    Changes to the next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+//const nextConfig = {
+  //output: 'export',
+  //eslint: {
+    //ignoreDuringBuilds: true,
+  //},
+  //images: { unoptimized: true },
+  //transpilePackages: ['three'],
+//};
+
+//module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignora errores de ESLint durante el build
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true // Desactiva la optimización de imágenes, útil con next export
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignora errores de TypeScript en tiempo de build
+  },
   transpilePackages: ['three'],
 };
 
