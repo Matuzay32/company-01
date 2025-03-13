d8de0ca55b01fedaae7ebadb648eb6081e0ca4ee
Matuzay32 <66391796+Matuzay32@users.noreply.github.com>
2025-03-13 16:48:37 -0300

    changes

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -531,7 +531,7 @@ export default function ServicesComponent() {
               </p>
               <a href={'/pricing'}>
                 <Button
-                  size="lg"
+                  size="md"
                   className="text-xl py-8 px-16 rounded-2xl bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 
                   sm:text-base sm:px-4 sm:py-2 md:text-lg md:px-8 md:py-4 lg:text-xl lg:px-16 lg:py-8"
                 >
