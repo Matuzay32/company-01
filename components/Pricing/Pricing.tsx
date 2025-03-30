0dd4d128e08bea648109059ab735204483a64b5c
Lucas <lucas.digenova@darwoft.com>
2025-03-30 17:03:42 -0300

    migrate dropdown components to other folder & fix pricing mobile broken & refactor mobile menu

--- components/Pricing/Pricing.tsx
+++ components/Pricing/Pricing.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from '@/components/ui/accordion';
+} from "@/components/ui/accordion";
 import {
   Card,
   CardContent,
@@ -13,8 +13,8 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { Check, ChevronDown, ChevronUp } from 'lucide-react';
+} from "@/components/ui/card";
+import { Check, ChevronDown, ChevronUp } from "lucide-react";
 import {
   Table,
   TableBody,
@@ -22,16 +22,16 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table';
+} from "@/components/ui/table";
 
-import { Button } from '@/components/ui/Buttons/v1/button';
-import CustomSection from '../ui/Others/CustomSection';
-import ModalCalendly from '../Modals/ModalCalendly';
-import { Switch } from '@/components/ui/switch';
-import { motion } from 'framer-motion';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import CreditCard3D from '../3d/CreditCard3D';
+import { Button } from "@/components/ui/Buttons/v1/button";
+import CreditCard3D from "../3d/CreditCard3D";
+import CustomSection from "../ui/Others/CustomSection";
+import ModalCalendly from "../Modals/ModalCalendly";
+import { Switch } from "@/components/ui/switch";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Pricing() {
   const [isAnnual, setIsAnnual] = useState(true);
@@ -43,78 +43,78 @@ export default function Pricing() {
 
   const plans = [
     {
-      name: 'Starter',
-      description: 'Perfect for your project start-up',
+      name: "Starter",
+      description: "Perfect for your project start-up",
       price: { monthly: 20, annual: 15 },
-      features: ['Website', 'Landing page', 'Web design', '5 hours support'],
+      features: ["Website", "Landing page", "Web design", "5 hours support"],
     },
     {
-      name: 'Pro',
-      description: 'For growing businesses and teams',
+      name: "Pro",
+      description: "For growing businesses and teams",
       price: { monthly: 39, annual: 30 },
       features: [
-        'Up to 3 customized websites',
-        '2 advanced landing pages with SEO optimization',
-        'Google Analytics integration',
-        'Web design',
-        'Projects planning',
-        '8 hours support',
-        'Outsourcing',
+        "Up to 3 customized websites",
+        "2 advanced landing pages with SEO optimization",
+        "Google Analytics integration",
+        "Web design",
+        "Projects planning",
+        "8 hours support",
+        "Outsourcing",
       ],
       popular: true,
     },
     {
-      name: 'Enterprise',
+      name: "Enterprise",
       description:
-        'Advanced scalability, innovation and premium support for your business.',
+        "Advanced scalability, innovation and premium support for your business.",
       price: { monthly: "Let's talk", annual: "Let's talk" },
       features: [
-        'Unlimited websites with advanced functionalities',
-        'Unlimited Landing pages optimized for conversion',
-        'Unlimited web designs',
-        'Advanced integrations with custom tools and APIs',
-        'Custom support',
-        'Tailor-made strategies: design, UX/UI and digital marketing consultancy',
-        'Ongoing management and maintenance of all projects',
-        'Outsourcing',
-        'Need anything else? contact us',
+        "Unlimited websites with advanced functionalities",
+        "Unlimited Landing pages optimized for conversion",
+        "Unlimited web designs",
+        "Advanced integrations with custom tools and APIs",
+        "Custom support",
+        "Tailor-made strategies: design, UX/UI and digital marketing consultancy",
+        "Ongoing management and maintenance of all projects",
+        "Outsourcing",
+        "Need anything else? contact us",
       ],
     },
   ];
 
   const featureComparison = [
     {
-      feature: 'Web sites',
+      feature: "Web sites",
       starter: true,
       pro: true,
       enterprise: true,
     },
     {
-      feature: 'Support',
+      feature: "Support",
       starter: true,
       pro: true,
       enterprise: true,
     },
     {
-      feature: 'Projects planning',
+      feature: "Projects planning",
       starter: false,
       pro: true,
       enterprise: true,
     },
     {
-      feature: 'Outsourcing',
+      feature: "Outsourcing",
       starter: false,
       pro: true,
       enterprise: true,
     },
     {
-      feature: 'Unlimited websites',
+      feature: "Unlimited websites",
       starter: false,
       pro: false,
       enterprise: true,
     },
     {
-      feature: 'Unlimited landing pages',
+      feature: "Unlimited landing pages",
       starter: false,
       pro: false,
       enterprise: true,
@@ -123,55 +123,55 @@ export default function Pricing() {
 
   const faqs = [
     {
-      question: 'What kind of digital products and services do we offer?',
+      question: "What kind of digital products and services do we offer?",
       answer:
-        'Our products and services include custom website design and development, optimized landing page design and development, custom software development, and Staff Augmentation+Outsourcing.',
+        "Our products and services include custom website design and development, optimized landing page design and development, custom software development, and Staff Augmentation+Outsourcing.",
     },
     {
       question:
-        'Can I request changes or adjustments after the project is finished?',
+        "Can I request changes or adjustments after the project is finished?",
       answer:
-        'Of course! We offer a post-delivery review period. In addition, we have maintenance and upgrade plans for those who wish to optimize or add new features in the future.',
+        "Of course! We offer a post-delivery review period. In addition, we have maintenance and upgrade plans for those who wish to optimize or add new features in the future.",
     },
     {
-      question: 'How involved can I be in the design and development process?',
+      question: "How involved can I be in the design and development process?",
       answer:
-        'We work closely with each client and throughout the project, providing regular updates and reviews to ensure that every detail is aligned with your expectations.',
+        "We work closely with each client and throughout the project, providing regular updates and reviews to ensure that every detail is aligned with your expectations.",
     },
     {
-      question: 'Do you offer customized services?',
+      question: "Do you offer customized services?",
       answer:
-        'Yes, each of our services is fully customizable. We adapt each project to the needs and style of your brand, ensuring a unique and authentic result.',
+        "Yes, each of our services is fully customizable. We adapt each project to the needs and style of your brand, ensuring a unique and authentic result.",
     },
     {
-      question: 'Can you handle the maintenance of my website or application?',
+      question: "Can you handle the maintenance of my website or application?",
       answer:
-        'We offer ongoing maintenance services so that your site or application is always up to date and running optimally. This includes technical support, content updates and security monitoring.',
+        "We offer ongoing maintenance services so that your site or application is always up to date and running optimally. This includes technical support, content updates and security monitoring.",
     },
     {
-      question: 'What is the cost of a landing page or website?',
+      question: "What is the cost of a landing page or website?",
       answer:
-        'Cost varies according to complexity and customization. We offer customized quotes for each project as well as pre-designed packages with flexible payment options.',
+        "Cost varies according to complexity and customization. We offer customized quotes for each project as well as pre-designed packages with flexible payment options.",
     },
     {
-      question: 'How is payment handled and do you have financing options?',
+      question: "How is payment handled and do you have financing options?",
       answer:
-        'To ensure transparency and convenience, we accept payments in different modalities, generally divided into milestones: a down payment, an interim payment and the final balance upon completion of the project. We also offer financing options for larger projects.',
+        "To ensure transparency and convenience, we accept payments in different modalities, generally divided into milestones: a down payment, an interim payment and the final balance upon completion of the project. We also offer financing options for larger projects.",
     },
     {
-      question: 'Why should I choose your agency and not another?',
+      question: "Why should I choose your agency and not another?",
       answer:
-        'We differentiate ourselves by our focus on innovation, customization and constant support throughout the process. We ensure that each project is unique and has a real impact on the growth of your business.',
+        "We differentiate ourselves by our focus on innovation, customization and constant support throughout the process. We ensure that each project is unique and has a real impact on the growth of your business.",
     },
   ];
   const renderCellContent = (value: boolean) => {
-    if (typeof value === 'boolean') {
-      return value ? <Check className="h-5 w-5 text-primary" /> : '—';
+    if (typeof value === "boolean") {
+      return value ? <Check className="h-5 w-5 text-primary" /> : "—";
     }
     return value;
   };
   return (
-    <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 bg-black">
+    <div className="md:mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 bg-black">
       {/* Pricing Header */}
       <motion.div
         className="text-center mb-16 mt-24"
@@ -188,7 +188,7 @@ export default function Pricing() {
         <div className="mt-8 flex items-center justify-center gap-4">
           <span
             className={`text-lg ${
-              !isAnnual ? 'text-primary font-semibold' : 'text-muted-foreground'
+              !isAnnual ? "text-primary font-semibold" : "text-muted-foreground"
             }`}
           >
             Monthly
@@ -200,10 +200,10 @@ export default function Pricing() {
           />
           <span
             className={`text-lg ${
-              isAnnual ? 'text-primary font-semibold' : 'text-muted-foreground'
+              isAnnual ? "text-primary font-semibold" : "text-muted-foreground"
             }`}
           >
-            yearly{' '}
+            yearly{" "}
           </span>
         </div>
       </motion.div>
@@ -219,7 +219,7 @@ export default function Pricing() {
           >
             <Card
               className={`flex flex-col h-full ${
-                plan.popular ? 'border shadow-lg scale-105' : ''
+                plan.popular ? "border shadow-lg md:scale-105" : ""
               }`}
             >
               <div
@@ -228,7 +228,7 @@ export default function Pricing() {
                 style={{
                   background: `linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(255, 255, 255, 0) 0%, #FBD38D 50%, rgba(0, 0, 0, 0) 100%)`,
                 }}
-              ></div>{' '}
+              ></div>{" "}
               <CardHeader>
                 {plan.popular && (
                   <div className="text-sm font-medium  mb-2  py-1 px-3 rounded-full w-fit">
@@ -241,13 +241,13 @@ export default function Pricing() {
               <CardContent className="flex-1">
                 <div className="mb-6">
                   <span className="text-5xl font-bold">
-                    {typeof plan.price[isAnnual ? 'annual' : 'monthly'] ===
-                    'number'
-                      ? `$${plan.price[isAnnual ? 'annual' : 'monthly']}`
-                      : plan.price[isAnnual ? 'annual' : 'monthly']}
+                    {typeof plan.price[isAnnual ? "annual" : "monthly"] ===
+                    "number"
+                      ? `$${plan.price[isAnnual ? "annual" : "monthly"]}`
+                      : plan.price[isAnnual ? "annual" : "monthly"]}
                   </span>
-                  {typeof plan.price[isAnnual ? 'annual' : 'monthly'] ===
-                    'number' && (
+                  {typeof plan.price[isAnnual ? "annual" : "monthly"] ===
+                    "number" && (
                     <span className="text-muted-foreground ml-2 text-lg">
                       /month
                     </span>
@@ -265,7 +265,7 @@ export default function Pricing() {
               <CardFooter>
                 <Button
                   className="w-full text-lg py-6 "
-                  variant={'outline'}
+                  variant={"outline"}
                   onClick={handleOpenModal}
                 >
                   Schedule a meet
@@ -351,9 +351,9 @@ export default function Pricing() {
           </p>
           <Button
             size="lg"
-            variant={'outline'}
+            variant={"outline"}
             className="text-lg py-6 px-8 "
-            onClick={() => router.push('/contact')}
+            onClick={() => router.push("/contact")}
           >
             Join
           </Button>
