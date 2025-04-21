b54fe6b35085f6f82f8b96ee21f665e8ed6cc197
Ezequiel Rey <eze.rey92@gmail.com>
2025-04-21 15:55:59 -0300

    feat: refactor Page component to use section elements for improved layout and scrolling behavior

--- app/services/web-desing/page.tsx
+++ app/services/web-desing/page.tsx
@@ -6,12 +6,22 @@ import DesignPrinciples from '@/components/sections/Principles/DesignPrinciples'
 
 export default function Page() {
   return (
-    <main className="bg-black">
-      <Hero />
-      <ShowcaseCarousel />
-      <ProductShowcase />
-      <ColorPalette />
-      <DesignPrinciples />
+    <main className="bg-black snap-y snap-mandatory h-screen overflow-y-scroll scroll-smooth">
+      <section className="snap-start h-screen">
+        <Hero />
+      </section>
+      <section className="snap-start h-screen">
+        <ShowcaseCarousel />
+      </section>
+      <section className="snap-start h-screen">
+        <ProductShowcase />
+      </section>
+      <section className="snap-start h-screen">
+        <ColorPalette />
+      </section>
+      <section className="snap-start h-screen">
+        <DesignPrinciples />
+      </section>
     </main>
   );
 }
